6d13b7251ec8eb82e9c9c04d95e02c92ccabde72
Ayaan Siddiqui <ayaanrsiddiqui@gmail.com>
2026-02-22 01:17:49 -0500

    Django Base Template

---
+++ dj-swe-app/lessonscheduler/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("This works! Team Number: A-12; Members: Lillie Paris, Jamar Oldacre, Elsa Norman, Sid Luthra, Ayaan Siddiqui")
