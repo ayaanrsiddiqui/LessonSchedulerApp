c5e0660750edd3e7ea54e3e78f9fbddcaac8ab46
xqd8qm <xqd8qm@virginia.edu>
2026-03-10 14:43:00 -0400

    Add google as a provider

--- djschedule/settings.py
+++ djschedule/settings.py
@@ -144,3 +144,15 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': ['profile', 'email'],
+        'AUTH_PARAMS': {'access_type': 'online'},
+        'APP': {
+            'client_id': os.getenv('CLIENT_ID'),
+            'secret': os.getenv('CLIENT_SECRET'),
+            'key': os.getenv('GOOGLE_KEY'),
+        }
+    }
+}
