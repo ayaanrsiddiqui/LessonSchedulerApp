c842194e6a6f268e86915807487c533fbbdeb163
xqd8qm <xqd8qm@virginia.edu>
2026-03-18 20:48:06 -0400

    add basic messaging implementation

--- djschedule/settings.py
+++ djschedule/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     'users',
     'lessonscheduler',
+    'messaging',
 ]
 
 MIDDLEWARE = [
---
+++ messaging/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Message
+from django.contrib.auth.models import User
+
+@login_required
+def inbox(request):
+    messages = Message.objects.filter(recipient=request.user)
+    return render(request, 'messaging/inbox.html', {'messages': messages})
+
+@login_required
+def send_message(request, username):
+    recipient = get_object_or_404(User, username=username)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            Message.objects.create(sender=request.user, recipient=recipient, content=content)
+            return redirect('inbox')
+    return render(request, 'messaging/send_message.html', {'recipient': recipient})
